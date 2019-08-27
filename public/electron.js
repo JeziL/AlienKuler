1c9c1a162dc4705a5f97ecddd6232369e393a5a5
Li <me@wangjinli.com>
2019-08-28 03:32:39 +0800

    dll work

--- public/electron.js
+++ public/electron.js
@@ -1,8 +1,8 @@
 const { app, BrowserWindow } = require('electron')
 const path = require('path')
 const { ipcMain } = require('electron');
-// const { LightFX } = require('./AlienFX/LightFX');
-// const { RESULT, LOCATION, Color } = require('./AlienFX/constants');
+const { LightFX } = require('./AlienFX/LightFX');
+const { RESULT, Color } = require('./AlienFX/constants');
 
 let mainWindow
 
@@ -16,7 +16,7 @@ function createWindow() {
             preload: path.join(__dirname, 'electron-wrapper.js')
         }
     })
-    mainWindow.removeMenu()
+    mainWindow.setMenu(null);
 
     if (process.env.DEVMODE) {
         mainWindow.loadURL('http://localhost:3000/')
@@ -46,28 +46,34 @@ app.on('activate', function () {
 })
 
 ipcMain.on('LFX_SETLIGHT', (event, arg) => {
-    // const libLightFX = new LightFX('C:\Windows\System32\LightFX.dll');
-    // const alignment = (arg.align === 'left') ? 0 : 1;
-    // const result = libLightFX.initialize();
-    // if (result === RESULT.SUCCESS) {
-    //     for (let i = 0; i < 4; i++) {
-    //         const swatch = arg.theme.swatches[i + alignment];
-    //         const color = new Color({
-    //             red: Math.ceil(swatch.values[0] * 255),
-    //             green: Math.ceil(swatch.values[1] * 255),
-    //             blue: Math.ceil(swatch.values[2] * 255),
-    //             brightness: 255
-    //         });
-    //         const result = libLightFX.setLightColor(0, i, color);
-    //         if (result !== RESULT.SUCCESS) {
-    //             event.reply('LFX_SETLIGHT_RESULT', result);
-    //             return;
-    //         }
-    //     }
-    //     const result = libLightFX.update();
-    //     event.reply('LFX_SETLIGHT_RESULT', result);
-    // } else {
-    //     event.reply('LFX_SETLIGHT_RESULT', result);
-    // }
-    console.log(arg);
+    const libLightFX = new LightFX(path.join(__dirname, 'LightFX.dll'));
+    const alignment = (arg.align === 'left') ? 0 : 1;
+    const result = libLightFX.initialize();
+    if (result === RESULT.SUCCESS) {
+        (async () => {
+            for (let i = 0; i < 4; i++) {
+                const swatch = arg.theme.swatches[i + alignment];
+                const color = new Color({
+                    red: Math.ceil(swatch.values[0] * 255),
+                    green: Math.ceil(swatch.values[1] * 255),
+                    blue: Math.ceil(swatch.values[2] * 255),
+                    brightness: 255
+                });
+                console.log(color);
+                let result = libLightFX.setLightColor(0, i, color);
+                if (result !== RESULT.SUCCESS) {
+                    event.sender.send('LFX_SETLIGHT_RESULT', result);
+                    return;
+                }
+                result = await libLightFX.update();
+                if (result !== RESULT.SUCCESS) {
+                    event.sender.send('LFX_SETLIGHT_RESULT', result);
+                    return;
+                }
+            }
+            event.sender.send('LFX_SETLIGHT_RESULT', RESULT.SUCCESS);
+        })();
+    } else {
+        event.sender.send('LFX_SETLIGHT_RESULT', result);
+    }
 });
