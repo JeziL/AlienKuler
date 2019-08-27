09551e753236c0a2fa27be6216562b584d9720ec
Li <me@wangjinli.com>
2019-08-27 20:35:59 +0800

    Search bar

--- public/electron.js
+++ public/electron.js
@@ -7,7 +7,9 @@ function createWindow() {
     mainWindow = new BrowserWindow({
         width: 960,
         height: 600,
+        title: "Alien Kuler"
     })
+    mainWindow.removeMenu()
 
     if (process.env.DEVMODE) {
         mainWindow.loadURL('http://localhost:3000/')
@@ -15,7 +17,7 @@ function createWindow() {
         mainWindow.loadFile(path.join(__dirname, '../build/index.html'))
     }
 
-    mainWindow.webContents.openDevTools()
+    // mainWindow.webContents.openDevTools()
     mainWindow.on('closed', function () {
         mainWindow = null
     })
--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import React, { Component } from 'react';
-import Button from 'antd/es/button';
+import { Input } from 'antd';
 import './App.css';
+const { Search } = Input;
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Button type="primary">Button</Button>
+        <div className="Search">
+          <Search placeholder="使用颜色、情境或关键字搜索，例如海洋、葡萄酒、月光、幸运、水..." onSearch={value => console.log(value)} enterButton />
+        </div>
       </div>
     );
   }
 }
-
-
 
 export default App;
