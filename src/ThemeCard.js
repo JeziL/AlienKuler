3446e72a113b14c56a1e95672d578d79a97ee4db
Li <me@wangjinli.com>
2019-08-27 23:18:34 +0800

    card setup

--- src/ThemeCard.js
+++ src/ThemeCard.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Card, Col, Row } from 'antd';
+import { Card, Icon, Rate, Row, Col, Statistic } from 'antd';
 const { Meta } = Card;
 
 class ThemeCard extends Component {
@@ -17,8 +17,29 @@ class ThemeCard extends Component {
             })
           }
         </div>
-      }>
-        <Meta title={theme.name} description={theme.author.name} />
+      }
+        actions={[
+          <Icon type="vertical-right" key="left" />,
+          <Icon type="vertical-left" key="right" />,
+          <Icon type="heart" key="heart" />
+        ]}>
+        <Meta title={theme.name} description={
+          <div>
+            <p>{theme.author.name}</p>
+            <Rate disabled defaultValue={theme.rating.overall} />
+            <span className="ant-rate-text">{`(${theme.rating.count})`}</span>
+            <Row style={{marginTop: '10px'}}>
+              <Col span={12}>
+                <Statistic title="" value={theme.like.count} prefix={<Icon type="like" />}
+                  valueStyle={{color: 'rgba(0, 0, 0, 0.45)', fontSize: '16px'}} />
+              </Col>
+              <Col span={12}>
+                <Statistic title="" value={theme.view.count} prefix={<Icon type="eye" />}
+                  valueStyle={{color: 'rgba(0, 0, 0, 0.45)', fontSize: '16px'}} />
+              </Col>
+            </Row>
+          </div>
+        } />
       </Card>
     );
   }
