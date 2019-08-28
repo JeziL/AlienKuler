bcc0fdb269571a44d30ddea3d5acc55495c40f3f
Li <me@wangjinli.com>
2019-08-28 20:11:59 +0800

    theme detail

--- src/ThemeCard.js
+++ src/ThemeCard.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
-import { Card, Icon, Rate, Row, Col, Statistic, message } from 'antd';
+import { Card, Icon, Rate, Row, Col, Statistic, message, Modal } from 'antd';
 const { Meta } = Card;
 const ipcRenderer = window.ipcRenderer;
 
 class ThemeCard extends Component {
-  state = { heart: false };
+  state = { heart: false, themeDetailVisible: false };
 
   componentDidMount() {
     this.setState({ heart: this.props.heart });
@@ -44,12 +44,21 @@ class ThemeCard extends Component {
     }
   }
 
+  static hexToColor(hex) {
+    const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    return result ? {
+      red: parseInt(result[1], 16),
+      green: parseInt(result[2], 16),
+      blue: parseInt(result[3], 16)
+    } : null;
+  }
+
   render() {
     const theme = this.props.theme;
 
     return (
       <Card bordered={false} cover={
-        <div className='Palette'>
+        <div className='Palette' onClick={() => { this.setState({ themeDetailVisible: true }) }}>
           {
             theme.swatches.map((swatch, i) => {
               return (
@@ -72,18 +81,42 @@ class ThemeCard extends Component {
             <p className="Author">{theme.author.name}</p>
             <Rate disabled defaultValue={theme.rating.overall} />
             <span className="ant-rate-text">{`(${theme.rating.count})`}</span>
-            <Row style={{marginTop: '10px'}}>
+            <Row style={{ marginTop: '10px' }}>
               <Col span={12}>
                 <Statistic title="" value={theme.like.count} prefix={<Icon type="like" />}
-                  valueStyle={{color: 'rgba(0, 0, 0, 0.45)', fontSize: '16px'}} />
+                  valueStyle={{ color: 'rgba(0, 0, 0, 0.45)', fontSize: '16px' }} />
               </Col>
               <Col span={12}>
                 <Statistic title="" value={theme.view.count} prefix={<Icon type="eye" />}
-                  valueStyle={{color: 'rgba(0, 0, 0, 0.45)', fontSize: '16px'}} />
+                  valueStyle={{ color: 'rgba(0, 0, 0, 0.45)', fontSize: '16px' }} />
               </Col>
             </Row>
           </div>
         } />
+        <Modal title={theme.name} visible={this.state.themeDetailVisible}
+          onCancel={() => { this.setState({ themeDetailVisible: false }) }} footer={null}
+          width={700}
+          bodyStyle={{ padding: 0 }}
+        >
+          <div className='Palette Palette-d'>
+            {
+              theme.swatches.map((swatch, i) => {
+                const { red, green, blue } = ThemeCard.hexToColor(swatch.hex);
+                const brightness = (red * 299 + green * 587 + blue * 114) / 1000;
+                const textColor = brightness > 125 ? 'rgba(0,0,0,0.65)' : 'rgba(255,255,255,0.85)';
+
+                return (
+                  <div className='Color Color-d' key={`${theme.id}-${i}-d`}
+                    style={{ backgroundColor: `#${swatch.hex}`, color: textColor }}
+                  >
+                    {`#${swatch.hex.toUpperCase()}`}<br />
+                    {`RGB ${red}, ${green}, ${blue}`}
+                  </div>
+                );
+              })
+            }
+          </div>
+        </Modal>
       </Card>
     );
   }
